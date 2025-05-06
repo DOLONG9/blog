f90e2a66a3e29c6f9261b1dd126faa65705131a0
이동구 <dg.lee@artistunited.com>
2025-05-06 21:49:25 +0900

    vault backup: 2025-05-06 21:49:25

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -48,3 +48,19 @@ export const defaultListPageLayout: PageLayout = {
   ],
   right: [],
 }
+
+<script src="https://giscus.app/client.js"
+        data-repo="DOLONG9/blog"
+        data-repo-id="R_kgDON3G-Mw"
+        data-category="[ENTER CATEGORY NAME HERE]"
+        data-category-id="[ENTER CATEGORY ID HERE]"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="preferred_color_scheme"
+        data-lang="ko"
+        crossorigin="anonymous"
+        async>
+</script>
