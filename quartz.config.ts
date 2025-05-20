60dbeef0660ba4c22effb66db8f859d82c483637
이동구 <dg.lee@artistunited.com>
2025-05-20 23:24:46 +0900

    제목 변경

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "☁️Doron's Blog",
+    pageTitle: "☁️Dorong's Blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
